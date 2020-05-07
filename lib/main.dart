ae700c3b89f746cae917df4dc62572c5d51c8a6f
Danielle Santos <daniellerodris@gmail.com>
2020-05-07 20:21:21 -0300

    Criando interface de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'Home.dart';
+import 'Login.dart';
 
 void main(){
 
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore.instance
-  .collection("usuarios")
-  .document("001")
-  .setData({"nome": "Jamilton"});
-
   runApp(MaterialApp(
-    home: Home(),
+    home: Login(),
+    theme: ThemeData(
+      primaryColor: Color(0xff075E54),
+      accentColor: Color(0xff25D366)
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
